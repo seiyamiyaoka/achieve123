26881ccda75c1fa1a3551b4f9058c7a450fe2a4e
seiyamiyaoka <seiyamiyaoka@gmail.com>
2015-11-06 13:30:32 +0000

    問い合わせvalidなし

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,8 +4,17 @@ class ContactsController < ApplicationController
   end
 
   def confirm
+    @contact = Contact.new(params_contact)
   end
 
   def thankyou
+    @contact = Contact.create(params_contact)
+    
+  end
+  
+  private
+  
+  def params_contact
+    params.require(:contact).permit(:name,:email,:content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/contact' => 'contacts#inquiry'
-
+  post '/contact/confirm' => 'contacts#confirm'
+  post '/contact/thankyou' => 'contacts#thankyou'
   resources :blogs
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
