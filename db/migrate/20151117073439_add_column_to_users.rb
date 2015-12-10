1dccdced6af3ba563c6e8043d155007a4c8f03bd
seiyamiyaoka <seiyamiyaoka@gmail.com>
2015-12-10 10:26:02 +0000

    hihi

--- db/migrate/20151117073439_add_column_to_users.rb
+++ db/migrate/20151117073439_add_column_to_users.rb
@@ -1,7 +1,7 @@
 class AddColumnToUsers < ActiveRecord::Migration
   def change
     add_column :users, :name, :string
-    add_column :users, :profile_image_url, :content
+    add_column :users, :profile_image_url, :string
     remove_column :users, :profile_image_url
 
   end
