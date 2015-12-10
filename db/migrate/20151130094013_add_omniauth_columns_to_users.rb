6f641b309276086f95efcad05c7d388513e623a9
seiyamiyaoka <seiyamiyaoka@gmail.com>
2015-12-10 10:11:58 +0000

    dbcolomun変更

--- db/migrate/20151130094013_add_omniauth_columns_to_users.rb
+++ db/migrate/20151130094013_add_omniauth_columns_to_users.rb
@@ -1,9 +1,9 @@
 class AddOmniauthColumnsToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :uid, :string, null: false, default: "",unique: true
-    add_column :users, :provider, :string, null: false, default: "",unique: true
+    add_column :users, :uid, :string, null: false, default: ""
+    add_column :users, :provider, :string, null: false, default: ""
     add_column :users, :image_url, :string
-
+  end
     add_index :users, [:uid, :provider], unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210054902) do
+ActiveRecord::Schema.define(version: 20151210100105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,5 +60,6 @@ ActiveRecord::Schema.define(version: 20151210054902) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
 
 end
